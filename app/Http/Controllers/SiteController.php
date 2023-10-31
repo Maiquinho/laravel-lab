214dc78f317ba973b1379f9f672d73c6018ae061
Michael Matheus <michaelmatheus2001@gmail.com>
2023-10-30 23:05:07 -0300

    Commit: passando dados para view

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -8,7 +8,19 @@ class SiteController extends Controller
 {
     // Lógica do controller...
     public function index(){
-        return view('welcome');
+        $name = 'Michael';
+        $lastName = 'Santos';
+        $birthDay = '23/04/2001';
+        $age = 90;
+
+        $data = [
+            'name' => $name,
+            'lastName' => $lastName,
+            'birthDay' => $birthDay,
+            'age' => $age
+        ];
+
+        return view('welcome', $data);
     }
 
     // Lógica do controller...
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,14 @@
-Bem vindo!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bem vindo</title>
+</head>
+<body>
+    <h1>Bem vindo {{ "$name $lastName" }}!</h1>
+    <p>Anivesário: {{ $birthDay }}</p>
+    <p>Idade: {{ $age }}</p>
+</body>
+</html>
